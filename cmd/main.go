2525ecae42e6dde914a465bad096d7ead4e861e7
Erick Müller <erick@em.pro.br>
2025-03-22 19:27:45 -0300

    primeira função

--- cmd/main.go
+++ cmd/main.go
@@ -2,9 +2,14 @@ package main
 
 import "fmt"
 
+func sayHello(name string) string {
+	r := fmt.Sprintf("Ola, %s", name)
+	return r
+}
+
 func main() {
 	fmt.Println("Qual seu nome? ")
 	var nome string
 	fmt.Scan(&nome)
-	fmt.Println("Ola, ", nome)
+	fmt.Println(sayHello(nome))
 }
